bc426ca14362b7f5214eb518c9df20a85f651503
Murilo Martins da Silva <murilo.silva@spread.com.br>
2022-02-04 17:23:24 -0300

    Fim - tratamento de exceções da api externa

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,17 +1,28 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
 botaoAdicionar.addEventListener("click", function(){
-    const xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
+    
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     
     xhr.addEventListener("load", function(){
-        const response = xhr.responseText;
-        const pacientes = JSON.parse(response);
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            const response = xhr.responseText;
+            const pacientes = JSON.parse(response);
+            
+            pacientes.forEach(function(paciente) {
+                addPacienteNaTabela(paciente);
+            });
         
-        pacientes.forEach( function(paciente) {
-            addPacienteNaTabela(paciente);
-        });
+        } else {
+            console.log("ERRO: " + xhr.status + ", " + xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
     })
-    
+
     xhr.send();
+
 })
